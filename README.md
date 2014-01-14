59c994b48d5ced52645a6b1befe99be04b7cf858
ingipingi <ingipingi@gmail.com>
2014-01-14 14:45:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,179 @@
+Selectator
+==========
+Selectator is a jQuery-based replacement for select boxes. It supports searching, and affects the original select box directly, which is used as the data container.
+[You can see a demo here](http://opensource.faroemedia.com/selectator).
+
+
+Usage
+-----
+###### include in head:
+```html
+<link rel="stylesheet" href="fm.selectator.jquery.css"/>
+<script src="jquery-2.0.3.min.js"></script>
+<script src="fm.tagator.jquery.js"></script>
+```
+
+###### to activate replacement:
+```javascript
+$('#selectBox').selectator();
+```
+
+###### if you want to change settings:
+```javascript
+$('#selectBox').selectator({
+    prefix: 'selectator_',        // CSS class prefix
+    height: 'auto',               // auto or element
+    useDimmer: false,             // dims the screen when result list is visible
+    showAllResultsOnFocus: false, // shows all results if input box is empty
+    searchCallback: function(){}, // Callback function when enter is pressed and 
+                                  //   no option is active in multi select box
+    labels: {
+        search: 'Search...'       // Placeholder text in search box in single select box
+    }
+});
+```
+
+###### Extra attributes for option tags
+By using `data-left`, `data-right` and `data-subtitle` attribute tags you can extend the information shown in the options. These can be styled through css, and are named `prefix_`title, `prefix_`left, `prefix_`right and `prefix_`subtitle. The data in the tags is pure html, so you can even put images there.
+
+```html
+<select id="selectBox">
+    <!-- Normal option tag -->
+    <option id="1">This is the title</option>
+    <!-- Extended option tag -->
+    <option id="2" data-left="This is the left section" data-right="This is the right section" data-subtitle="This is the section under the title">This is the title</option>
+</select>
+```
+It will be displayed something like this this:
+<table>
+    <tr>
+        <td rowspan="2">
+            Left
+        </td>
+        <td>
+            Title
+        </td>
+        <td rowspan="2">
+            Right
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Subtitle
+        </td>
+    </tr>
+</table>
+
+
+CSS classes
+-----------
+Here is a list of all the css classes
+
+Class                         | Description
+----------------------------- | ------------------------------------------------------------------------------
+selectator                    | This is the new select box. It has some extra classes called `single` and `multiple`, which tell if it is a multiple selection or single selection select box. And also `results-visible` and `results-hidden` which tell if the results list is visible or not.
+`prefix_`chosen_items         | The holder for the chosen items.
+`prefix_`chosen_item          | The holder for the chosen item.
+`prefix_`chosen_item_title    | The title of the chosen item.
+`prefix_`chosen_item_left     | The left section of the chosen item.
+`prefix_`chosen_item_right    | The right section of the chosen item.
+`prefix_`chosen_item_subtitle | The bottom section of the chosen item.
+`prefix_`chosen_item_remove   | The remove button for the chosen item.
+`prefix_`input                | This is the input box for the selectator. This is used together with `results-visible` or `results-hidden` to show and style it differently if it is a multiple selection box or a single selection box.
+`prefix_`textlength           | This is used to calculate the size of the input box for the multiple selection box.
+`prefix_`results              | The results list holder. This is used together with `results-visible` or `results-hidden` to show or hide the results.
+`prefix_`group_header         | This is the group title option.
+`prefix_`group                | This is the group options holder.
+`prefix_`option               | This is a result option. It has an extra class called `active` which tells if the option is the active one.
+`prefix_`option_title         | The title of the result option.
+`prefix_`option_left          | The left section of the result option.
+`prefix_`option_right         | The right section of the result option.
+`prefix_`option_subtitle      | The bottom section of the result option.
+`prefix_`dimmer               | This is the dimmer
+
+
+DOM Structure
+-------------
+* dimmer
+* selectator: *containing the `single`|`multiple` class and the `results-visible`|`results-hidden` class*
+    * textlength
+    * chosen_items
+        * chosen_item
+            * chosen_item_left
+            * chosen_item_right
+            * chosen_item_title
+            * chosen_item_subtitle
+            * chosen_item_remove
+        * chosen_item...
+    * input
+    * results
+        * group_header
+        * group
+            * option: *containing the `active` class*
+                * option_left
+                * option_right
+                * option_title
+                * option_subtitle
+            * option...
+        * option: *containing the `active` class*
+            * option_left
+            * option_right
+            * option_title
+            * option_subtitle
+        * option...
+
+
+jQuery methods
+--------------
+Method             | Description
+------------------ | -----------
+refreshChosenItems | This method is used internally by the plugin, but you can also call it manually, it is used to refresh the plugin. A scenario where this would be useful is if the data in the original select box is changed by some other script.
+destroy            | This method is used to remove the instance of the plugin from the select box and restore it to its original state.
+
+
+###### Method usage
+```javascript
+$('#selectBox').selectator('refreshChosenItems');
+```
+or 
+```javascript
+$('#selectBox').selectator('destroy');
+```
+
+
+Browser compatibility
+---------------------
+* IE ???
+* Chrome 8+
+* Firefox ???
+* Safari ???
+* Opera ???
+
+
+Internationalization
+--------------------
+Selectator supports language by setting labels through the plugin options.
+
+
+Copyright and license
+---------------------
+The MIT License (MIT)
+
+Copyright (c) 2013 Faroe Media
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ fm.tagator.jquery.js
@@ -0,0 +1,492 @@
+/*
+ Tagator jQuery Plugin
+ A plugin to make input elements, tag holders
+ version 1.0, Jan 13th, 2014
+ by Ingi P. Jacobsen
+
+ The MIT License (MIT)
+
+ Copyright (c) 2014 Faroe Media
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+(function($) {
+	$.tagator = function (element, options) {
+		var defaults = {
+			prefix: 'tagator_',
+			height: 'auto',
+			useDimmer: false,
+			showAllResultsOnFocus: false,
+			autocomplete: []
+		};
+	
+		var plugin = this;
+		var selected_index = -1;
+		var box_element = null;
+		var tags_element = null;
+		var input_element = null;
+		var textlength_element = null;
+		var results_element = null;
+		var key = {
+			backspace: 8,
+			enter: 13,
+			escape: 27,
+			left: 37,
+			up: 38,
+			right: 39,
+			down: 40,
+			comma: 188
+		};
+		plugin.settings = {};
+
+		
+		
+		// INITIALIZE PLUGIN
+		plugin.init = function () {
+			plugin.settings = $.extend({}, defaults, options);
+
+			//// ================== CREATE ELEMENTS ================== ////
+			// dimmer
+			if (plugin.settings.useDimmer) {
+				if ($('#' + plugin.settings.prefix + 'dimmer').length === 0) {
+					var dimmer_element = document.createElement('div');
+					$(dimmer_element).attr('id', plugin.settings.prefix + 'dimmer');
+					$(dimmer_element).hide();
+					$(document.body).prepend(dimmer_element);
+				}
+			}
+			// box element
+			box_element = document.createElement('div');
+			if (element.id !== undefined) {
+				$(box_element).attr('id', plugin.settings.prefix + element.id);
+			}
+			$(box_element).addClass('tagator results-hidden');
+			$(box_element).css({
+				width: $(element).css('width'),
+				padding: $(element).css('padding'),
+				position: 'relative'
+			});
+			if (plugin.settings.height === 'element') {
+				$(box_element).css({
+					height: $(element).outerHeight() + 'px'
+				});
+			}
+			$(element).after(box_element);
+			$(element).hide();
+			// textlength element
+			textlength_element = document.createElement('span');
+			$(textlength_element).addClass(plugin.settings.prefix + 'textlength');
+			$(textlength_element).css({
+				position: 'absolute',
+				visibility: 'hidden'
+			});
+			$(box_element).append(textlength_element);
+			// tags element
+			tags_element = document.createElement('div');
+			$(tags_element).addClass(plugin.settings.prefix + 'tags');
+			$(box_element).append(tags_element);
+			// input element
+			input_element = document.createElement('input');
+			$(input_element).addClass(plugin.settings.prefix + 'input');
+			$(input_element).width(20);
+			$(input_element).attr('autocomplete', 'false');
+			$(box_element).append(input_element);
+			// results element
+			results_element = document.createElement('ul');
+			$(results_element).addClass(plugin.settings.prefix + 'results');
+
+			$(box_element).append(results_element);
+
+			//// ================== BIND ELEMENTS EVENTS ================== ////
+			// source element
+			$(element).change(function () {
+				refreshTags();
+			});
+			// box element
+			$(box_element).bind('focus', function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+				showOptions();
+				$(input_element).focus();
+			});
+			$(box_element).bind('mousedown', function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+				input_element.focus();
+				if (input_element.setSelectionRange) {
+					input_element.focus();
+					input_element.setSelectionRange(input_element.value.length, input_element.value.length);
+				} else if (input_element.createTextRange) {
+					var range = input_element.createTextRange();
+					range.collapse(true);
+					range.moveEnd('character', input_element.value.length);
+					range.moveStart('character', input_element.value.length);
+					range.select();
+				}
+			});
+			$(box_element).bind('mouseup', function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+			});
+			$(box_element).bind('click', function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+				if (plugin.settings.showAllResultsOnFocus) {
+					//showResults();
+					searchOptions();
+				}
+				input_element.focus();
+			});
+			$(box_element).bind('dblclick', function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+				input_element.focus();
+				input_element.select();
+			});
+			// input element
+			$(input_element).bind('click', function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+			});
+			$(input_element).bind('dblclick', function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+			});
+			$(input_element).bind('keydown', function (e) {
+				e.stopPropagation();
+				var keyCode = e.keyCode || e.which;
+				switch (keyCode) {
+					case key.up:
+						e.preventDefault();
+						if (selected_index > -1) {
+							selected_index = selected_index - 1;
+						} else {
+							selected_index = $(results_element).find('.' + plugin.settings.prefix + 'option').length - 1;
+						}
+						refreshActiveOption();
+						scrollToActiveOption();
+						break;
+					case key.down:
+						e.preventDefault();
+						if (selected_index < $(results_element).find('.' + plugin.settings.prefix + 'option').length - 1) {
+							selected_index = selected_index + 1;
+						} else {
+							selected_index = -1;
+						}
+						refreshActiveOption();
+						scrollToActiveOption();
+						break;
+					case key.escape:
+						e.preventDefault();
+						break;
+					case key.comma:
+						e.preventDefault();
+						if (selected_index === -1) {
+							if ($(input_element).val() !== '') {
+								addTag($(input_element).val());
+							}
+						}
+						resizeInput();
+						break;
+					case key.enter:
+						e.preventDefault();
+						if (selected_index !== -1) {
+							selectOption();
+						} else {
+							if ($(input_element).val() !== '') {
+								addTag($(input_element).val());
+							}
+						}
+						resizeInput();
+						break;
+					case key.backspace:
+						if (input_element.value === '') {
+							$(element).val($(element).val().substring(0, $(element).val().lastIndexOf(',')));
+							$(element).trigger('change');
+							searchOptions();
+						}
+						resizeInput();
+						break;
+					default:
+						resizeInput();
+						break;
+				}
+			});
+			$(input_element).bind('keyup', function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+				var keyCode = e.keyCode || e.which;
+				if (keyCode === key.escape || keyCode === key.enter) {
+					hideOptions();
+				} else if (keyCode < 37 || keyCode > 40) {
+					searchOptions();
+				}
+				if ($(box_element).hasClass('results-hidden') && (keyCode === key.left || keyCode === key.right || keyCode === key.up || keyCode === key.down)) {
+					searchOptions();
+				}
+				resizeInput();
+			});
+			$(input_element).bind('focus', function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+				if (!$(results_element).is(':empty') || plugin.settings.showAllResultsOnFocus) {
+					searchOptions();
+					showOptions();
+				}
+			});
+			$(input_element).bind('blur', function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+				hideOptions();
+			});
+			refreshTags();
+		};
+		
+		
+		
+		// RESIZE INPUT
+		var resizeInput = function () {
+			textlength_element.innerHTML = input_element.value;
+			$(input_element).css({ width: ($(textlength_element).width() + 20) + 'px' });
+		};
+
+		
+
+		// REFRESH TAGS
+		plugin.refresh = function () {
+			refreshTags();
+		};
+		var refreshTags = function () {
+			$(tags_element).empty();
+			var tags = $(element).val().split(',');
+			$.each(tags, function (key, value) {
+				if (value !== '') {
+					var tag_element = document.createElement('div');
+					$(tag_element).addClass(plugin.settings.prefix + 'tag');
+					$(tag_element).html(value);
+					// remove button
+					var button_remove_element = document.createElement('div');
+					$(button_remove_element).data('text', value);
+					$(button_remove_element).addClass(plugin.settings.prefix + 'tag_remove');
+					$(button_remove_element).bind('mousedown', function (e) {
+						e.preventDefault();
+						e.stopPropagation();
+					});
+					$(button_remove_element).bind('mouseup', function (e) {
+						e.preventDefault();
+						e.stopPropagation();
+						removeTag($(this).data('text'));
+						$(element).trigger('change');
+					});
+					$(button_remove_element).html('X');
+					$(tag_element).append(button_remove_element);
+					// clear
+					var clear_element = document.createElement('div');
+					clear_element.style.clear = 'both';
+					$(tag_element).append(clear_element);
+	
+					$(tags_element).append(tag_element);
+				}
+			});
+			searchOptions();
+		};
+		
+		// REMOVE TAG FROM ORIGINAL ELEMENT
+		var removeTag = function (text) {
+			var tagsBefore = $(element).val().split(',');
+			var tagsAfter = [];
+			$.each(tagsBefore, function (key, value) {
+				if (value !== text && value !== '') {
+					tagsAfter.push(value);
+				}
+			});
+			$(element).val(tagsAfter.join(','));
+		};
+		
+		// CHECK IF TAG IS PRESENT
+		var hasTag = function (text) {
+			var tags = $(element).val().split(',');
+			var hasTag = false;
+			$.each(tags, function (key, value) {
+				if ($.trim(value) === $.trim(text)) {
+					hasTag = true;
+				}
+			});
+			return hasTag;
+		};
+		
+		// ADD TAG TO ORIGINAL ELEMENT
+		var addTag = function (text) {
+			if (!hasTag(text)) {
+				$(element).val($(element).val() + ($(element).val() !== '' ? ',' : '') + text);
+				$(element).trigger('change');
+			}
+			$(input_element).val('');
+			box_element.focus();
+			hideOptions();
+		};
+
+
+
+		// RESULTS SEARCH METHOD
+		var searchOptions = function () {
+			$(results_element).empty();
+			if (input_element.value.replace(/\s/g, '') !== '' || plugin.settings.showAllResultsOnFocus) {
+				var optionsArray = [];
+				$.each(plugin.settings.autocomplete, function (key, value) {
+					if (value.toLowerCase().indexOf(input_element.value.toLowerCase()) !== -1) {
+						if (!hasTag(value)) {
+							optionsArray.push(value);
+						}
+					}
+				});
+				generateOptions(optionsArray);
+			}
+			if ($(input_element).is(':focus')) {
+				if (!$(results_element).is(':empty')) {
+					showOptions();
+				} else {
+					hideOptions();
+				}
+			} else {
+				hideOptions();
+			}
+			selected_index = -1;
+		};
+
+		// GENERATE RESULTS
+		var generateOptions = function (optionsArray) {
+			var index = -1;
+			$(optionsArray).each(function (key, value) {
+				index++;
+				var option = createOption(value, index);
+				$(results_element).append(option);
+			});
+			refreshActiveOption();
+		};
+
+		// CREATE RESULT OPTION
+		var createOption = function (text, index) {
+			// holder li
+			var option = document.createElement('li');
+			$(option).data('index', index);
+			$(option).data('text', text);
+			$(option).html(text);
+			$(option).addClass(plugin.settings.prefix + 'option');
+			if (this.selected) {
+				$(option).addClass('active');
+			}
+
+			// BIND EVENTS
+			$(option).bind('mouseover', function (e) {
+				e.stopPropagation();
+				e.preventDefault();
+				selected_index = index;
+				refreshActiveOption();
+			});
+			$(option).bind('mousedown', function (e) {
+				e.stopPropagation();
+				e.preventDefault();
+			});
+			$(option).bind('click', function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+				selectOption();
+			});
+
+
+			return option;
+		};
+
+		// SHOW RESULTS AND DIMMER
+		var showOptions = function () {
+			$(box_element).removeClass('results-hidden').addClass('results-visible');
+			if (plugin.settings.useDimmer) {
+				$('#' + plugin.settings.prefix + 'dimmer').show();
+			}
+			$(results_element).css('top', ($(box_element).outerHeight()-2) + 'px');
+			if ($(box_element).hasClass('single')) {
+				selected_index = $(results_element).find('.' + plugin.settings.prefix + 'option').index($(results_element).find('.' + plugin.settings.prefix + 'option.active'));
+			}
+			scrollToActiveOption();
+		};
+
+		// HIDE RESULTS AND DIMMER
+		var hideOptions = function () {
+			$(box_element).removeClass('results-visible').addClass('results-hidden');
+			if (plugin.settings.useDimmer) {
+				$('#' + plugin.settings.prefix + 'dimmer').hide();
+			}
+		};
+
+		// REFRESH ACTIVE IN RESULTS METHOD
+		var refreshActiveOption = function () {
+			$(results_element).find('.active').removeClass('active');
+			if (selected_index !== -1) {
+				$(results_element).find('.' + plugin.settings.prefix + 'option').eq(selected_index).addClass('active');
+			}
+		};
+
+		// SCROLL TO ACTIVE OPTION IN RESULTS LIST
+		var scrollToActiveOption = function () {
+			var $active_element = $(results_element).find('.' + plugin.settings.prefix + 'option.active');
+			if ($active_element.length > 0) {
+				$(results_element).scrollTop($(results_element).scrollTop() + $active_element.position().top - $(results_element).height()/2 + $active_element.height()/2);
+			}
+
+		};
+
+		// SELECT ACTIVE ITEM
+		var selectOption = function () {
+			addTag($(results_element).find('.' + plugin.settings.prefix + 'option').eq(selected_index).data('text'));
+		};
+
+
+
+		// REMOVE PLUGIN AND REVERT SELECT ELEMENT TO ORIGINAL STATE
+		plugin.destroy = function () {
+			$(box_element).remove();
+			$.removeData(element, 'tagator');
+			$(element).show();
+			if ($('.tagator').length === 0) {
+				$('#' + plugin.settings.prefix + 'dimmer').remove();
+			}
+		};
+
+		// Initialize plugin
+		plugin.init();
+	};
+	
+	$.fn.tagator = function(options) {
+		options = options !== undefined ? options : {};
+		return this.each(function () {
+			if (typeof(options) === 'object') {
+				if (undefined === $(this).data('tagator')) {
+					var plugin = new $.tagator(this, options);
+					$(this).data('tagator', plugin);
+				}
+			} else if ($(this).data('tagator')[options]) {
+				$(this).data('tagator')[options].apply(this, Array.prototype.slice.call(arguments, 1));
+			} else {
+				$.error('Method ' + options + ' does not exist in $.tagator');
+			}
+		});
+	};
+
+}(jQuery));
